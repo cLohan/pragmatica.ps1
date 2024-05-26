334d87a346a3b04d5a064e52808e4946db4a138a
cLohan <82850047+cLohan@users.noreply.github.com>
2024-05-26 12:27:27 -0300

    Update pragmatica.ps1

--- pragmatica.ps1
+++ pragmatica.ps1
@@ -1,5 +1,5 @@
-$TimesToRun = 10
-$RunTimeP = 1	# Time in minutes
+$TimesToRun = 60
+$RunTimeP = 60	# Time in minutes
 $From = "luccasmachado001@outlook.com"
 $Pass = "hbzgnuqauqooxcbq"
 $To = "carlosalberto58@protonmail.com"
